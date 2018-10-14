c09df1e9757e6f3a6316fc689ffefbbd9e7440bf
Shreyansh Khajanchi <shreyansh_k@live.com>
2018-10-14 15:34:55 +0530

    correct VirtualBox declaration
    NixOS 18.09 changed how VirtualBox extensions are enabled.
    Update accordingly.

--- configuration.nix
+++ configuration.nix
@@ -78,8 +78,10 @@
 
 	# enable virtualisation
 	virtualisation.docker.enable = true;
-	virtualisation.virtualbox.host.enable = true; # hosted hypervisor
-	nixpkgs.config.virtualbox.enableExtensionPack = true;
+	virtualisation.virtualbox.host = { # hosted hypervisor
+		enable = true;
+		enableExtensionPack = true;
+	};
 
 	# Some programs need SUID wrappers, can be configured further or are
 	# started in user sessions.
