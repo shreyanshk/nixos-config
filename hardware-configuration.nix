c541221be43e394862aaddd22cb1666f3ebb2ef3
Shreyansh Khajanchi <shreyansh_k@live.com>
2019-04-15 20:50:44 +0530

    hardware: update i915 config on update to NixOS 19.03

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -7,10 +7,8 @@
 	];
 
 	# i915: enable better power savings
-	# enable_guc_loading=1 and enable_guc_submission=1 are merged into enable_guc=3 since Linux 4.16+
-	# update this when kernel changes to 4.16+
 	boot.extraModprobeConfig = ''
-		options i915 enable_dc=1 enable_fbc=1 enable_rc6=1 semaphores=1 disable_power_well=0 enable_guc_loading=1 enable_guc_submission=1
+		options i915 enable_dc=1 enable_fbc=1 enable_rc6=1 semaphores=1 disable_power_well=0 enable_guc=3
 	'';
 	boot.extraModulePackages = [ ];
 	boot.initrd.availableKernelModules = [
